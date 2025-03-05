03f017a58326e489c63606d3b3b0bd6704f8f22c
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2025-03-05 14:29:36 +0200

    save

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Card from  './components/Card';
 import Carousel from './components/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from './components/Accordion';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         <Card />
         <Card />
       </div>
+      <div className='Accordion'>
+        <Accordion />
+        </div>
     </div>
   );
 }
